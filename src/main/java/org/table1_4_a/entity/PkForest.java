0f3dc3f7806c41f0873fda3bb58b3bef6ac0a4d2
gabriele <daniele.romagnoli@geo-solutions.it>
2012-07-06 09:31:21 +0200

    #5

---
+++ src/main/java/org/table1_4_a/entity/PkForest.java
@@ -0,0 +1,28 @@
+package org.table1_4_a.entity;
+
+import java.io.Serializable;
+import javax.persistence.Embeddable;
+@Embeddable
+public class PkForest implements Serializable{
+    /** serialVersionUID */
+    private static final long serialVersionUID = 1L;
+    private String userId;
+    private int year;
+    public PkForest(){}
+    public PkForest(String userId, int year) {
+        this.userId = userId;
+        this.year = year;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/main/java/org/table1_4_b/entity/PkCategory.java
@@ -0,0 +1,29 @@
+package org.table1_4_b.entity;
+
+
+import java.io.Serializable;
+import javax.persistence.Embeddable;
+@Embeddable
+public class PkCategory implements Serializable{
+    /** serialVersionUID */
+    private static final long serialVersionUID = 1L;
+    private String userId;
+    private String category;
+    public PkCategory(){}
+    public PkCategory(String userId, String category) {
+        this.userId = userId;
+        this.category = category;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setYear(String category) {
+        this.category = category;
+    }
+}
